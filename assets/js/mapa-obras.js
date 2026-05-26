ebc9164a6700787249e6cf8da9f435c19f7826a2
amigosdeluchito-afk <amigosdeluchito@gmail.com>
2026-05-25 22:29:46 -0500

    fase 2 casi 31 mapa

--- assets/js/mapa-obras.js
+++ assets/js/mapa-obras.js
@@ -67,9 +67,9 @@ window.initMapEngine = async function(container) {
         }
 
         const texts = [
-            "UNIVERSO OBRAS",
-            "FUERZA TACNA",
-            "EL FUTURO ES AHORA"
+            "BIENVENIDO A TACNA",
+            "EL FUTURO SE CONSTRUYE HOY",
+            "OBRAS FUERZA TACNA"
         ];
 
         container.style.setProperty('display', 'flex', 'important');
