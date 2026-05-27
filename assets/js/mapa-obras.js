ea866bb7ea4532119784ce4efced85894f3bc56b
amigosdeluchito-afk <amigosdeluchito@gmail.com>
2026-05-27 11:14:45 -0500

    mapa 24

--- assets/js/mapa-obras.js
+++ assets/js/mapa-obras.js
@@ -458,7 +458,7 @@ window.initMapEngine = async function(container) {
                 'text-transform': 'uppercase',
                 'text-letter-spacing': 0.05,
                 'text-variable-anchor': ['right', 'left', 'top', 'bottom'],
-                'text-radial-offset': 1,
+                'text-radial-offset': 0.9,
                 'text-justify': 'auto',
                 'text-max-width': 12,
                 'text-padding': 20 
