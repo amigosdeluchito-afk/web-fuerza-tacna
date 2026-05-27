625dee91005fa4cad23e294e2b80fae8df239825
amigosdeluchito-afk <amigosdeluchito@gmail.com>
2026-05-27 11:54:15 -0500

    mapa 30

--- assets/js/mapa-obras.js
+++ assets/js/mapa-obras.js
@@ -473,7 +473,7 @@ window.initMapEngine = async function(container) {
                 'text-radial-offset': -0.2,
                 'text-justify': 'auto',
                 'text-max-width': 12,
-                'text-padding': 15 
+                'text-padding': 45 
             },
             paint: {
                 'text-color': '#111111',
