949f2d1dd7aea677b881391da8c467d6a18a5573
amigosdeluchito-afk <amigosdeluchito@gmail.com>
2026-06-02 23:24:01 -0500

    ia 12

--- assets/panel-admin-universo/config.php
+++ assets/panel-admin-universo/config.php
@@ -48,7 +48,7 @@ const DB_NAME = 'tacnwddf_fuerza';
 
 function get_db_connection() {
     try {
-        $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
+        $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     } catch (PDOException $e) {
