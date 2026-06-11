499c12b60146a549bd539a0b512f47047e1c4c67
amigosdeluchito-afk <amigosdeluchito@gmail.com>
2026-06-10 23:17:48 -0500

    obras 34

--- assets/js/mapa-obras.js
+++ assets/js/mapa-obras.js
@@ -185,6 +185,7 @@ window.initMapEngine = async function(container) {
     // Creamos un archivo "virtual" local en la memoria del navegador (Blob) 
     // que importe el código externo de forma 100% legal y permitida por el navegador.
     // =================================================================================
+    /*
     if (window.maplibregl) {
         try {
             const workerCode = "importScripts('https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl-worker.js');";
@@ -194,6 +195,7 @@ window.initMapEngine = async function(container) {
             console.warn("[Mapa] Fallo al crear el Blob Worker. Dependiendo del motor interno.");
         }
     }
+    */
 
     const map = new maplibregl.Map({
         container: mapEl,
